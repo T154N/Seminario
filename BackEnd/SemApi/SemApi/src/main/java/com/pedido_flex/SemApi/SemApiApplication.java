be78920218087edf5436574e5712be428e78cb60
T154N <santiiariias2001@gmail.com>
2024-08-31 14:15:41 -0300

    entorno backen
    Diseñado con spring initializr

---
+++ BackEnd/SemApi/SemApi/src/main/java/com/pedido_flex/SemApi/SemApiApplication.java
@@ -0,0 +1,13 @@
+package com.pedido_flex.SemApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemApiApplication.class, args);
+	}
+
+}
---
+++ BackEnd/SemApi/SemApi/src/test/java/com/pedido_flex/SemApi/SemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedido_flex.SemApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

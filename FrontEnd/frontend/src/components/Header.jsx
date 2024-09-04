22070c7ac5ae6f3cff13050932af2762a3149973
joaco molla <joamolla18@gmail.com>
2024-09-04 15:35:37 -0300

    Primera version del login

---
+++ FrontEnd/frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './header.css';
+import logo from '../images/Header Icons/logoPF.png';
+
+export function Header() {
+    return(
+        <header className="header text-white p-3">
+            <img src={logo} alt='Logo' className='logo'></img>
+            <img src={logo} alt='Logo' className='logo'></img> # Cambiar por PedidoFlex
+        </header>
+    )
+}
---
+++ FrontEnd/frontend/src/components/login/Login.jsx
@@ -0,0 +1,13 @@
+import {useForm} from "react-hook-form";
+import React from "react-hook-form";
+import {useState, useEffect} from "react";
+import './login.css';
+
+export function Login() {
+
+    return(
+        <div className="login">
+            <h1>Login</h1>
+        </div>
+    )
+}

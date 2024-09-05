128721b9ffed9be45a3a8858651b6a4ac3f08962
joaco molla <joamolla18@gmail.com>
2024-09-05 14:37:01 -0300

    falta alinear los botones al final

--- FrontEnd/frontend/src/components/Header.jsx
+++ FrontEnd/frontend/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 import './header.css';
-import logo from '../images/Header Icons/logoPF.png';
+import logo from '../images/Header Icons/PFlogo.png';
+import PFLogo from '../images/Header Icons/PedidoFlex.png';
 
 export function Header() {
     return(
         <header className="header text-white p-3">
             <img src={logo} alt='Logo' className='logo'></img>
-            <h1>PedidoFlex</h1>
+            <img src={PFLogo} alt='PedidoFlex' className='pflogo'></img>
         </header>
     )
 }

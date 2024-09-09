ac0d55f54690988da02a88da7d2e59be99b1b538
T154N <santiiariias2001@gmail.com>
2024-09-09 19:42:43 -0300

    bdd
    .

---
+++ BackEnd/wsPedidoFlex/wsPedidoFlex/src/main/java/com/pedido_flex/wsPedidoFlex/model/Cliente.java
@@ -0,0 +1,28 @@
+package com.pedido_flex.wsPedidoFlex.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table (name = "clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "email")
+    private String email;
+
+
+}

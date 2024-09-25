03f5a3d6a321c90b742d1b7ea16f999f0193cca0
jppirra <pirrajuanpablo@gmail.com>
2024-09-25 15:02:39 -0300

    Cambios en usuarios y login provisorio para que peguen desde el front

---
+++ BackEnd/wsPedidoFlex/wsPedidoFlex/src/main/java/com/pedido_flex/wsPedidoFlex/Utils/Constants.java
@@ -0,0 +1,62 @@
+package com.pedido_flex.wsPedidoFlex.Utils;
+
+/**
+ * Constantes de uso general
+ *
+ */
+public class Constants {
+
+    public static final String LOGIN_EXITO_MESSAGE = "Login Exitoso";
+    public static final String LOGIN_ERROR_MESSAGE = "Ocurrió un error al autenticarse: ";
+    public static final String LOGIN_ERROR_GENERAL = "Error general: ";
+//    public static final String TOKEN_SPLIT = "\\.";
+//
+//    public static final String BEARER_TOKEN = "Bearer ";
+//    public static final String LOGIN_ERROR_MESSAGE = "Error de autenticacion: ";
+//    public static final String LOGIN_ERROR_MISSING_ID = "Usuario con configuración pendiente: id";
+//    public static final String LOGIN_ERROR_GENERAL = "Error general: ";
+//    public static final String EMPTY_STRING = "";
+//    public static final String REASON_ID_PERSONA = "IDPERSONA";
+//    public static final String REASON_APPS = "APPS";
+//    public static final String REASON_ROLES = "ROLES";
+//    public static final String REASON_AREA = "AREA";
+//    public static final String REASON_ID_PERSONA_MESSAGE = "Usuario con configuración de IDPERSONA pendiente.";
+//    public static final String REASON_APPS_MESSAGE = "Usuario con configuracion de APPS pendiente.";
+//    public static final String REASON_ROLES_MESSAGE = "Usuario con configuración de ROLES pendiente.";
+//    public static final String REASON_AREA_MESSAGE = "Usuario con configuración de AREA pendiente.";
+//    public static final String BODY_ITEM_FILE = "file";
+//    public static final String BODY_ITEM_USER = "user";
+//    public static final String BODY_ITEM_TOKEN = "token";
+//    public static final String REASON_USERPIC_VALIDATION_ERROR = "No se validó la foto del usuario";
+//    public static final String EXCEPTION_MESSAGE_RESPONSE_WSLOGINTOOLS =
+//            "No pudo ser logueado, el usuario no es quien dice ser";
+//    public static final String CONEXION_ACEPTADA = "Allowed";
+//    public static final String CONEXION_RECHAZADA = "Refused";
+//    public static final String MESSAGE_ERROR_SAVE_DATA_ACCESS_LOG =
+//            "No se pudo guardar en la tabla access_log, los datos del usuario: ";
+//    public static final String[] IP_HEADER_CANDIDATES = {
+//            "X-Forwarded-For",
+//            "Proxy-Client-IP",
+//            "WL-Proxy-Client-IP",
+//            "HTTP_X_FORWARDED_FOR",
+//            "HTTP_X_FORWARDED",
+//            "HTTP_X_CLUSTER_CLIENT_IP",
+//            "HTTP_CLIENT_IP",
+//            "HTTP_FORWARDED_FOR",
+//            "HTTP_FORWARDED",
+//            "HTTP_VIA",
+//            "REMOTE_ADDR"
+//    };
+//    public static final String LOCALHOST_IP_ADDRESS = "127.0.0.1";
+//    public static final String UNKNOWN = "unknown";
+//    public static final String COMMA = ",";
+//    public static final String LOGGER_INFO_OK_IMAGEN = "Obtuvo bien la imagen de loginTools";
+//    public static final String MESSAGE_ERROR_GET_SESSION_TOKEN = "Ocurrio un error al obtener token desde Checkout Api";
+//    public static final String TOKEN_REASON="TOKEN REASON";
+//    public static final String MENSAJE_TOKEN_ERROR =
+//            "Hubo un error al recuperar el token desde LoginTools y Checkout Api";
+//    public static final String DOT = ".";
+//    public static final String MESSAGE_PASSWORD_CHANGE_SUCCESS = "Contraseña cambiada con éxito!";
+//    public static final Long HTTP_STATUS_OK = 200L;
+}
+

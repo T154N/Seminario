874bd9059f31bab334a671fedda0acffb5798257
Nicolás Carreras <Nicolascarreras023@gmail.com>
2024-09-29 18:36:57 -0300

    Carrito

---
+++ FrontEnd/frontend/src/components/Header.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import './header.css';
+import PFLogo from '../images/Header Icons/PedidoFlex.png';
+import { Carrito } from './carrito/Carrito';  // Importa el componente Carrito
+import { useState } from "react";
+
+export function Header() {
+    const [total, setTotal] = useState(0);
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/principal');
+    };
+
+    const actualizarTotal = (total) => {
+        setTotal(total);
+    }
+
+
+    return (
+        <div>
+            <header className="header text-white">
+                <img
+                    src={PFLogo}
+                    alt='PedidoFlex'
+                    className='pflogo'
+                    onClick={handleLogoClick}
+                    style={{ cursor: 'pointer' }}
+                />
+                <div className="header bg-dark text-white d-flex justify-content-between align-items-center p-3">
+                    <button
+                        className="btn btn-primary"
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasScrolling"
+                        aria-controls="offcanvasScrolling"
+                    >
+                        Carrito
+                    </button>
+
+                    <div className="offcanvas offcanvas-end custom-offcanvas-width"
+                        data-bs-scroll="true"
+                        data-bs-backdrop="false"
+                        tabIndex="-1"
+                        id="offcanvasScrolling"
+                        aria-labelledby="offcanvasScrollingLabel"
+                    >
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasScrollingLabel">
+                                Carrito de Compras
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="offcanvas"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+
+                        {/* Contenedor principal del cuerpo del carrito */}
+                        <div className="offcanvas-body carrito-body">
+                            <Carrito
+                            actualizarTotal={actualizarTotal} />
+                        </div>
+
+                        {/* Footer fijo */}
+                        <div className="carrito-footer">
+                            <h5>Total: ${total}</h5> {/* Puedes reemplazar este total dinámicamente */}
+                            <button className="btn btn-primary">Iniciar Compra</button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+

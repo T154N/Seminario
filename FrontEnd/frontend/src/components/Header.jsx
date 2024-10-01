d815713ebfde49e80d737f90551554b6e749572c
Nicolás Carreras <Nicolascarreras023@gmail.com>
2024-09-30 23:46:34 -0300

    Actualizacion del componenete carrito
    Ahora toda la lógica del carrito esta en el componente carrito y te muestra un mensaje cuando no hay productos

--- FrontEnd/frontend/src/components/Header.jsx
+++ FrontEnd/frontend/src/components/Header.jsx
@@ -3,20 +3,13 @@ import { useNavigate } from "react-router-dom";
 import './header.css';
 import PFLogo from '../images/Header Icons/PedidoFlex.png';
 import { Carrito } from './carrito/Carrito';
-import { useState } from "react";
 
 export function Header() {
-    const [total, setTotal] = useState(0);
     const navigate = useNavigate();
 
     const handleLogoClick = () => {
         navigate('/principal');
     };
-
-    const actualizarTotal = (total) => {
-        setTotal(total);
-    }
-
 
     return (
         <div>
@@ -29,51 +22,9 @@ export function Header() {
                     style={{ cursor: 'pointer' }}
                 />
                 <div className="header bg-dark text-white d-flex justify-content-between align-items-center p-3">
-                    <button
-                        className="btn btn-primary"
-                        type="button"
-                        data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasScrolling"
-                        aria-controls="offcanvasScrolling"
-                    >
-                        Carrito
-                    </button>
-
-                    <div className="offcanvas offcanvas-end custom-offcanvas-width"
-                        data-bs-scroll="true"
-                        data-bs-backdrop="false"
-                        tabIndex="-1"
-                        id="offcanvasScrolling"
-                        aria-labelledby="offcanvasScrollingLabel"
-                    >
-                        <div className="offcanvas-header">
-                            <h5 className="offcanvas-title" id="offcanvasScrollingLabel">
-                                Carrito de Compras
-                            </h5>
-                            <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="offcanvas"
-                                aria-label="Close"
-                            ></button>
-                        </div>
-
-                       
-                        <div className="offcanvas-body carrito-body">
-                            <Carrito
-                            actualizarTotal={actualizarTotal} />
-                        </div>
-
-                    
-                        <div className="carrito-footer">
-                            <h5>Total: ${total}</h5> 
-                            <button className="btn btn-primary">Iniciar Compra</button>
-                        </div>
-                    </div>
+                    <Carrito />
                 </div>
             </header>
         </div>
     );
 }
-
-

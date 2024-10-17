7236c0553afacf633787923d7e9f1ef074fd00c1
julyguzman <julyguzman0904@gmail.com>
2024-10-16 22:46:20 -0300

    info cheta

--- FrontEnd/frontend/src/components/info/Info.jsx
+++ FrontEnd/frontend/src/components/info/Info.jsx
@@ -5,26 +5,36 @@ export function Info() {
         <div className="container-page">
             <div className="info-row">
                 <div className='info-col'>
-                    <h1>¿FORMAS DE PAGO?</h1>
+                    <h1>¿CUÁNDO SE PUEDEN REALIZAR LOS PEDIDOS?</h1>
+                    <p>Los pedidos pueden realizarse todos los días de la semana</p>
+                </div>
+                <div className='info-col'>
+                    <h1>¿CUÁNDO SE REALIZAN LAS ENTREGAS DE LOS PEDIDOS?</h1>
+                    <p>Las entregas se realizan los dias:</p>
                     <ul className="payment-list">
-                        <li>EFECTIVO</li><p>Los pagos en efectivo deben realizarse al moneto de la entrega del pedido</p>
-                        <li>TRANSFERENCIA</li><p>Las transferencia deben realizarse al mometo de realizar la compra via web y mandand el comprobante via WhastsApp</p>
-                        <li>TARJETA</li><p>Los productos abonados con tarjeta tiene un recaro del 10%</p>
+                        <li>MIÉRCOLES:</li><p>Todos aquellos pedidos realizados y abonados los días Viernes, Sábado, Domingo y Lunes.</p>
+                        <li>SÁBADO:</li><p>Todos aquellos pedidos realizados y abonados los días Martes, Miércoles y Jueves.</p>
                     </ul>
                 </div>
                 <div className='info-col'>
-                    <h1>ENTREGAS</h1>
-                    <p>Las entregas se realizaran los dias:</p>
-                    <ul className="payment-list">
-                        <li>MARTES</li><p>Tods aquellos pedidos abonados hasta el dia Lunes a las 00hs</p>
-                        <li>SABADO</li><p>Todos aquellos pedidos abonados hasta el via Viernes a las 00hs</p>
-                    </ul>
+                    <h1>¿HASTA CUÁNDO PUEDO MODIFICAR MI PEDIDO?</h1>
+                    <p>El pedido se puede modificar hasta las 11:59am del día del armado del pedido.</p>
                 </div>
                 <div className='info-col'>
-                    <h1>DEVOLUCIONES</h1>
-                    <p>No se realizara devoluciones de dinero, se entregarn en un futuro auqellos productos señados</p>
+                    <h1>¿CUÁLES SON LAS FORMAS DE PAGO?</h1>
+                    <ul className="payment-list">
+                        <li>EFECTIVO</li><p>Los pagos en efectivo deben realizarse al momento de la entrega del pedido</p>
+                        <li>TRANSFERENCIA</li><p>Las transferencias deben realizarse al momento de realizar la compra vía web y mandar el comprobante vía WhastsApp</p>
+                    </ul>
                 </div>
             </div>
         </div>
     );
 }
+
+
+
+
+
+
+

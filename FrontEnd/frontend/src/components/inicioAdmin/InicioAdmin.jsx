923e51ea5993b15fa36dcbed3d8bd3a8c1708933
T154N <santiiariias2001@gmail.com>
2024-10-29 00:53:13 -0300

    cambio botones
    gg: Juli ponete la 10

--- FrontEnd/frontend/src/components/inicioAdmin/InicioAdmin.jsx
+++ FrontEnd/frontend/src/components/inicioAdmin/InicioAdmin.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './inicioAdmin.css'
+import './inicioAdmin.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export function InicioAdmin() {
     const [content, setContent] = useState('Bienvenido');
     const [busqueda, setBusqueda] = useState('');
+
     const handleBusquedaChange = (e) => {
         setBusqueda(e.target.value);
     };
@@ -14,11 +17,13 @@ export function InicioAdmin() {
         { id: 2, nombre: 'María', apellido: 'Perez', email: 'maria@example.com', telefono: '3517437189', cuit:'27447402756', domicilio:'Leuvuco 5291'},
         { id: 3, nombre: 'Carlos',apellido: 'Perez', email: 'carlos@example.com', telefono: '3517437189', cuit:'27447402756', domicilio:'Leuvuco 5291'},
     ];
+
     const productosActivos = [
-        { id: 1, nombre: 'Arros', descripcion: 'abcdfghijasjdnjd', estado: 'alta', categoria: 'alimentos', url: 'https::/', precio: '$1000' },
-        { id: 2, nombre: 'Alfajor', descripcion: 'abcdfghijasjdnjd', estado: 'alta', categoria: 'alimentos', url: 'https::/',precio: '$500' },
-        { id: 3, nombre: 'Gaseosa', descripcion: 'abcdfghijasjdnjd', estado: 'alta', categoria: 'alimentos', url: 'https::/',precio: '$750' },
+        { id: 1, nombre: 'Arroz', descripcion: 'abcdfghijasjdnjd', estado: 'alta', categoria: 'alimentos', url: 'https::/', precio: '$1000' },
+        { id: 2, nombre: 'Alfajor', descripcion: 'abcdfghijasjdnjd', estado: 'alta', categoria: 'alimentos', url: 'https::/', precio: '$500' },
+        { id: 3, nombre: 'Gaseosa', descripcion: 'abcdfghijasjdnjd', estado: 'alta', categoria: 'alimentos', url: 'https::/', precio: '$750' },
     ];
+
     const categoriasActivas = [
         { id: 1, nombre: 'Filos', estado:'activo', url: 'https'},
         { id: 2, nombre: 'Copetin', estado:'activo', url: 'https'},
@@ -27,167 +32,181 @@ export function InicioAdmin() {
 
     const handleButtonClick = (newContent) => {
         if (newContent === 'Clientes') {
-            const filteredClientes = clientesActivos.filter(cliente => 
+            const filteredClientes = clientesActivos.filter(cliente =>
                 cliente.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
                 cliente.apellido.toLowerCase().includes(busqueda.toLowerCase()) ||
                 cliente.domicilio.toLowerCase().includes(busqueda.toLowerCase())
             );
-            
+
             setContent(
                 <div>
                     <h2>Clientes Activos</h2>
-                    
-                    <button className="btn btn-primary mb-3 d-flex justify-content-between" onClick={handleAlta}>Alta de Cliente</button>
+                    <button className="btn btn-primary mb-3" onClick={handleAlta}>Alta de Cliente</button>
                     <div className="mb-3">
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder="Buscar cliente..." 
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Buscar cliente..."
                             value={busqueda}
                             onChange={handleBusquedaChange}
                         />
                     </div>
-                    <div className='table-responsive'>
+                    <div className="table-responsive">
                         <table className="table table-striped">
                             <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nombre</th>
-                                    <th>Apellido</th>
-                                    <th>Email</th>
-                                    <th>Telefono</th>
-                                    <th>CUIT</th>
-                                    <th>Domicilio</th>
-                                    <th>Accion</th>
-                                </tr>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Email</th>
+                                <th>Teléfono</th>
+                                <th>CUIT</th>
+                                <th>Domicilio</th>
+                                <th>Acción</th>
+                            </tr>
                             </thead>
                             <tbody>
-                                {filteredClientes.map(cliente => (
-                                    <tr key={cliente.id}>
-                                        <td>{cliente.id}</td>
-                                        <td>{cliente.nombre}</td>
-                                        <td>{cliente.apellido}</td>
-                                        <td>{cliente.email}</td>
-                                        <td>{cliente.telefono}</td>
-                                        <td>{cliente.cuit}</td>
-                                        <td>{cliente.domicilio}</td>
-                                        <td>
-                                            <button className="btn btn-warning btn-sm me-2" onClick={() => handleModify(cliente.id)}>Modificar</button>
-                                            <button className="btn btn-danger btn-sm" onClick={() => handleDelete(cliente.id)}>Baja</button>
-                                        </td>
-                                    </tr>
-                                ))}
+                            {filteredClientes.map(cliente => (
+                                <tr key={cliente.id}>
+                                    <td>{cliente.id}</td>
+                                    <td>{cliente.nombre}</td>
+                                    <td>{cliente.apellido}</td>
+                                    <td>{cliente.email}</td>
+                                    <td>{cliente.telefono}</td>
+                                    <td>{cliente.cuit}</td>
+                                    <td>{cliente.domicilio}</td>
+                                    <td>
+                                        <button className="btn btn-warning btn-sm me-2" onClick={() => handleModify(cliente.id)}>
+                                            <FontAwesomeIcon icon={faEdit} />
+                                        </button>
+                                        <button className="btn btn-danger btn-sm" onClick={() => handleDelete(cliente.id)}>
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
                             </tbody>
                         </table>
                     </div>
                 </div>
             );
         }
+
         if (newContent === 'Productos') {
-            const filteredproductos = productosActivos.filter(producto => 
+            const filteredProductos = productosActivos.filter(producto =>
                 producto.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
                 producto.estado.toLowerCase().includes(busqueda.toLowerCase()) ||
                 producto.categoria.toLowerCase().includes(busqueda.toLowerCase())
             );
+
             setContent(
                 <div>
                     <h2>Productos Activos</h2>
-                    <button className="btn btn-primary mb-3 d-flex justify-content-between" onClick={handleAlta}>Alta de Producto</button>
+                    <button className="btn btn-primary mb-3" onClick={handleAlta}>Alta de Producto</button>
                     <div className="mb-3">
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder="Buscar producto..." 
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Buscar producto..."
                             value={busqueda}
                             onChange={handleBusquedaChange}
                         />
                     </div>
-                    <div className='table-resposive'>
+                    <div className="table-responsive">
                         <table className="table table-striped">
                             <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nombre</th>
-                                    <th>Descripción</th>
-                                    <th>Estado</th>
-                                    <th>Categoría</th>
-                                    <th>URL</th>
-                                    <th>Precio</th>
-                                    <th>Accion</th>
-                                    
-                                </tr>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Descripción</th>
+                                <th>Estado</th>
+                                <th>Categoría</th>
+                                <th>URL</th>
+                                <th>Precio</th>
+                                <th>Acción</th>
+                            </tr>
                             </thead>
                             <tbody>
-                                {filteredproductos.map(producto => (
-                                    <tr key={producto.id}>
-                                        <td>{producto.id}</td>
-                                        <td>{producto.nombre}</td>
-                                        <td>{producto.descripcion}</td>
-                                        <td>{producto.estado}</td>
-                                        <td>{producto.categoria}</td>
-                                        <td>{producto.url}</td>
-                                        <td>{producto.precio}</td>
-                                        <td>
-                                            <button className="btn btn-warning btn-sm me-2" onClick={() => handleModify(producto.id)}>Modificar</button>
-                                            <button className="btn btn-danger btn-sm" onClick={() => handleDelete(producto.id)}>Baja</button>
-                                        </td>
-                                    </tr>
-                                ))}
+                            {filteredProductos.map(producto => (
+                                <tr key={producto.id}>
+                                    <td>{producto.id}</td>
+                                    <td>{producto.nombre}</td>
+                                    <td>{producto.descripcion}</td>
+                                    <td>{producto.estado}</td>
+                                    <td>{producto.categoria}</td>
+                                    <td>{producto.url}</td>
+                                    <td>{producto.precio}</td>
+                                    <td>
+                                        <button className="btn btn-warning btn-sm me-2" onClick={() => handleModify(producto.id)}>
+                                            <FontAwesomeIcon icon={faEdit} />
+                                        </button>
+                                        <button className="btn btn-danger btn-sm" onClick={() => handleDelete(producto.id)}>
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
                             </tbody>
                         </table>
                     </div>
                 </div>
             );
         }
+
         if (newContent === 'Categorias') {
-            const filteredcategorias = categoriasActivas.filter(categoria => 
+            const filteredCategorias = categoriasActivas.filter(categoria =>
                 categoria.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
                 categoria.estado.toLowerCase().includes(busqueda.toLowerCase())
             );
+
             setContent(
                 <div>
-                    <h2>Categorias Activas</h2>
-                    <button className="btn btn-primary mb-3 d-flex justify-content-between" onClick={handleAlta}>Alta de Categoría</button>
+                    <h2>Categorías Activas</h2>
+                    <button className="btn btn-primary mb-3" onClick={handleAlta}>Alta de Categoría</button>
                     <div className="mb-3">
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder="Buscar categoria..." 
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Buscar categoría..."
                             value={busqueda}
                             onChange={handleBusquedaChange}
                         />
                     </div>
-                    <div className='table-responsive'>
+                    <div className="table-responsive">
                         <table className="table table-striped">
                             <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nombre</th>
-                                    <th>Estado</th>
-                                    <th>URL</th>
-                                    <th>Accion</th>
-
-                                </tr>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Estado</th>
+                                <th>URL</th>
+                                <th>Acción</th>
+                            </tr>
                             </thead>
                             <tbody>
-                                {filteredcategorias.map(categoria => (
-                                    <tr key={categoria.id}>
-                                        <td>{categoria.id}</td>
-                                        <td>{categoria.nombre}</td>
-                                        <td>{categoria.estado}</td>
-                                        <td>{categoria.url}</td>
-                                        <td>
-                                            <button className="btn btn-warning btn-sm me-2" onClick={() => handleModify(categoria.id)}>Modificar</button>
-                                            <button className="btn btn-danger btn-sm" onClick={() => handleDelete(categoria.id)}>Baja</button>
-                                        </td>
-                                    </tr>
-                                ))}
+                            {filteredCategorias.map(categoria => (
+                                <tr key={categoria.id}>
+                                    <td>{categoria.id}</td>
+                                    <td>{categoria.nombre}</td>
+                                    <td>{categoria.estado}</td>
+                                    <td>{categoria.url}</td>
+                                    <td>
+                                        <button className="btn btn-warning btn-sm me-2" onClick={() => handleModify(categoria.id)}>
+                                            <FontAwesomeIcon icon={faEdit} />
+                                        </button>
+                                        <button className="btn btn-danger btn-sm" onClick={() => handleDelete(categoria.id)}>
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
                             </tbody>
                         </table>
                     </div>
                 </div>
             );
         }
+
         if (newContent === 'Inicio') {
             setContent(
                 <div>
@@ -199,20 +218,16 @@ export function InicioAdmin() {
     };
 
     const handleModify = (id) => {
-        alert(`Modificación de cliente con ID: ${id}`);
-        // lógica para modificar el cliente.
+        alert(`Modificación de elemento con ID: ${id}`);
     };
 
     const handleDelete = (id) => {
-        alert(`Baja de cliente con ID: ${id}`);
-        // lógica para eliminar el cliente.
+        alert(`Baja de elemento con ID: ${id}`);
     };
 
     const handleAlta = () => {
-        alert("Formulario de alta de cliente"); 
-        // logica alta
+        alert("Formulario de alta");
     };
-
 
     return (
         <div className="container-fluid">
@@ -228,11 +243,9 @@ export function InicioAdmin() {
                 </div>
 
                 <div className="col-12 col-md-10 contenido">
-                    <div>{content}</div> 
+                    <div>{content}</div>
                 </div>
             </div>
         </div>
     );
 }
-
-
